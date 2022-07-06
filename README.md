cd0bae4187ea6ed568e518916cc4e49d7525f35d
Felix Nilo <felixjesusnilob@gmail.com>
2022-07-06 09:52:07 -0400

    last commit

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ Y ademas, redireccionemos al dashboard al ingresar una ruta vacia en nuestro app
 
 ## Finalmente, agregemos un tag a nuestro push en git para documentar la version de la aplicacion.
 
-Para ello, ingresemos por consola `git tag -a v1.1 -m "Rutas listas"`
+Para ello, ingresemos por consola `git tag -a v1.1 -m "Rutas listas"` para crear un nuevo tag asociado a nuestro commit.
+Ahora, hagamos commit y push de nuestro codigo a nuestro repositorio. Es importante agregar el decorador --tags para que con el push se cree el tag que hemos creado.
--- adminpro/src/app/app-routing.module.ts
+++ adminpro/src/app/app-routing.module.ts
@@ -6,16 +6,14 @@ import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component'
 import { AuthRoutingModule } from './auth/auth.routing';
 
 const routes: Routes = [
-  {
-    // path: '/dashboard' Pages Routing
-    // path: '/progress' Pages Routing
-    // path: '/grafica1' Pages Routing
-    // path: '' Pages Routing
-    // path: '/login' Auth Routing
-    // path: '/register' Auth Routing
-    path: '**',
-    component: NopagefoundComponent,
-  },
+  // path: '/dashboard' Pages Routing
+  // path: '/progress' Pages Routing
+  // path: '/grafica1' Pages Routing
+  // path: '' Pages Routing
+  // path: '/login' Auth Routing
+  // path: '/register' Auth Routing
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NopagefoundComponent },
 ];
 
 @NgModule({
