b7c024dd36cd8f5bcbb7fe0e966ff0921754ef35
Felix Nilo <felixjesusnilob@gmail.com>
2022-07-06 09:36:02 -0400

    last commit

--- adminpro/src/app/app-routing.module.ts
+++ adminpro/src/app/app-routing.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { PagesRoutingModule } from './pages/pages.routing';
 
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { AuthRoutingModule } from './auth/auth.routing';
 
-  { path: '**', component: NopagefoundComponent },
+const routes: Routes = [
+  {
+    // path: '/dashboard' Pages Routing
+    // path: '/progress' Pages Routing
+    // path: '/grafica1' Pages Routing
+    // path: '' Pages Routing
+    // path: '/login' Auth Routing
+    // path: '/register' Auth Routing
+    path: '**',
+    component: NopagefoundComponent,
+  },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes), PagesRoutingModule],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule,
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
