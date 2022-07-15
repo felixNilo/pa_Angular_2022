d735508a225103c072043209ff3409e99d9b9fdc
felixNilo <54825678+felixNilo@users.noreply.github.com>
2022-07-15 17:24:51 -0400

    Update usuarios.js

--- adminpro-back/routes/usuarios.js
+++ adminpro-back/routes/usuarios.js
@@ -16,7 +16,7 @@ router.post(
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("password", "El password es obligatorio").not().isEmpty(),
     check("email", "El email es obligatorio").isEmail(),
-    validarCampos(),
+    validarCampos,
   ],
   createUsuario
 );
